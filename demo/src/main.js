23f6e99f0e5be45eba258c5338666a1ec6d95487
Yair Levy <supraniti.inbox@gmail.com>
2021-02-23 20:07:33 +0200

    /

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueDndZone from 'vue-dnd-zone'
+Vue.use(VueDndZone)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
