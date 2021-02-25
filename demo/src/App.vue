8a54b356c44408f8bef1f382fd4714d441b74dab
Yair Levy <supraniti.inbox@gmail.com>
2021-02-26 00:23:35 +0200

    /

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -51,32 +51,29 @@
       <v-tabs-items v-model="tab">
         <v-tab-item>
           <v-card class="elevation-5">
-            <v-card-title>
-              About
-            </v-card-title>
-              <dnd-zone handle-class="handle">
-                <v-card-text>
-                  <dnd-container :dnd-model="about.children" dnd-id="about" class="row" :vertical-search="$vuetify.breakpoint.xs">
-                    <template v-for="item in about.children">
-                      <dnd-item :key="item.id" :dnd-id="item.id" :dnd-model="item">
-                        <v-col cols="12" :sm="item.cols" class="">
-                          <v-card :dark="item.dark" :color="item.color">
-                            <v-card-actions>
-                              <v-icon class="handle">mdi-drag</v-icon>
-                              <v-spacer></v-spacer>
-                            </v-card-actions>
-                            <v-card-text class="body-1 pt-0">
-                              <p class="caption my-2" v-text="item.title"></p>
-                              <p class="my-2" v-for="(p,i) in item.text" :key="i" v-text="p"></p>
-                              <div v-for="(p,i) in item.html" :key="i" v-html="p"></div>
-                            </v-card-text>
-                          </v-card>
-                        </v-col>
-                      </dnd-item>
-                    </template>
-                  </dnd-container>
-                </v-card-text>
-              </dnd-zone>
+            <dnd-zone handle-class="handle" :native-scroll="false">
+              <v-card-text>
+                <dnd-container :dnd-model="about.children" dnd-id="about" class="row" :vertical-search="$vuetify.breakpoint.xs">
+                  <template v-for="item in about.children">
+                    <dnd-item :key="item.id" :dnd-id="item.id" :dnd-model="item">
+                      <v-col cols="12" :sm="item.cols" class="">
+                        <v-card :dark="item.dark" :color="item.color">
+                          <v-card-actions>
+                            <v-icon class="handle">mdi-drag</v-icon>
+                            <v-spacer></v-spacer>
+                          </v-card-actions>
+                          <v-card-text class="body-1 pt-0" style="opacity:1">
+                            <p class="title my-2" v-text="item.title"></p>
+                            <p class="my-2" v-for="(p,i) in item.text" :key="i" v-text="p"></p>
+                            <div v-for="(p,i) in item.html" :key="i" v-html="p"></div>
+                          </v-card-text>
+                        </v-card>
+                      </v-col>
+                    </dnd-item>
+                  </template>
+                </dnd-container>
+              </v-card-text>
+            </dnd-zone>
             <v-card-actions>
               Next
             </v-card-actions>
@@ -136,7 +133,23 @@ export default {
           dark:false,
           cols:4,
           title:'Installation',
-          html:[`<kbd>npm install vue-dnd-zone</kbd><p class="mt-2 mb-0 caption">main.js</p><code>import VueDndZone from 'vue-dnd-zone'` + `<BR>` +  `import 'vue-dnd-zone/vue-dnd-zone.css'</code>`]
+          html:[`<kbd>npm install vue-dnd-zone</kbd><p class="mt-2 mb-0 caption">main.js</p><code>import VueDndZone from 'vue-dnd-zone'` + `<br/>` +  `import 'vue-dnd-zone/vue-dnd-zone.css'</code>`]
+        },
+        {
+          id:4,
+          color:'white',
+          dark:false,
+          cols:4,
+          title:'Installation',
+          html:[`<kbd>npm install vue-dnd-zone</kbd><p class="mt-2 mb-0 caption">main.js</p><code>import VueDndZone from 'vue-dnd-zone'` + `<br/>` +  `import 'vue-dnd-zone/vue-dnd-zone.css'</code>`]
+        },
+        {
+          id:5,
+          color:'white',
+          dark:false,
+          cols:4,
+          title:'Installation',
+          html:[`<kbd>npm install vue-dnd-zone</kbd><p class="mt-2 mb-0 caption">main.js</p><code>import VueDndZone from 'vue-dnd-zone'` + `<br/>` +  `import 'vue-dnd-zone/vue-dnd-zone.css'</code>`]
         }
       ]
     }
